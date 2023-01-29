99dea3aaf1b982e038fe495e94b5aedb32b59318
Alexandros Solanos <alex@uizard.io>
2023-01-29 22:15:52 +0200

    Changes

--- _next/static/6S7gMa7BTvBVWUHDxNnIo/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/154feaea638d63e9.css","static/chunks/pages/index-bb013afd70b98c75.js"],"/_error":["static/chunks/pages/_error-1fd6c3782812bbc4.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
